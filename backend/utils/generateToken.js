6c57567ee53b42cb6284af134ea6a23a926cef1b
Maher Suleyman <maher.suleyman.ms@gmail.com>
2023-08-30 19:08:17 -0400

    Add JWT generator & save cookie for [BE]

---
+++ backend/utils/generateToken.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+
+const generateToken = (res, userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
+    sameSite: "strict",
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days = (30 days * 24 hours * 60 mins * 60 secs * 1000 milliseconds )
+  });
+};
+
+export default generateToken;
