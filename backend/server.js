e1470d7991787286a1b4ca4f7c980349d6ad9f5c
Maher Suleyman <maher.suleyman.ms@gmail.com>
2023-08-30 16:58:16 -0400

    Initialize the backend app using Node.js & express

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => res.send("Server is ready."));
+
+app.listen(port, () => console.log(`Server started at port ${port}`));
