739edd9e4fd5c93b66be2e68f51abc9a3553010c
Maher Suleyman <maher.suleyman.ms@gmail.com>
2023-09-02 11:29:19 -0400

    Set up "React Router" for [FE]

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 import { FaSignInAlt, FaUserPlus } from "react-icons/fa";
 
 const Header = () => {
@@ -6,16 +7,22 @@ const Header = () => {
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">Yadi</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>Yadi</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/login">
-                <FaSignInAlt /> Sign In
-              </Nav.Link>
-              <Nav.Link href="/register">
-                <FaUserPlus /> Sign Up
-              </Nav.Link>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <FaSignInAlt /> Sign In
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/register">
+                <Nav.Link>
+                  <FaUserPlus /> Sign Up
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
+import HomerScreen from "./screens/HomeScreen";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomerScreen />} />
+    </Route>
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
